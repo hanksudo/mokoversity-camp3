8afda01f5b6b78e2c68a2484a6ff309461565c0d
Hank Wang <drapho@gmail.com>
2013-10-20 08:00:51 +0800

    Use JS Module Pattern. fixes #7

--- main.js
+++ main.js
@@ -1,16 +1,25 @@
-(function() {
-    var canvas = document.getElementById('game');
-    var ctx = canvas.getContext('2d');
+var gameModule = (function() {
 
-    var ballX = Math.floor(Math.random() * 250);
-    var ballY = Math.floor(Math.random() * 200);
-    var ballR = Math.floor(Math.random() * 100);
+    function start() {
+        var canvas = document.getElementById('game');
+        var ctx = canvas.getContext('2d');
 
-    canvas.width = 480;
-    canvas.here = 320;
+        var ballX = Math.floor(Math.random() * 250);
+        var ballY = Math.floor(Math.random() * 200);
+        var ballR = Math.floor(Math.random() * 100);
 
-    ctx.fillStyle = 'black';
-    ctx.beginPath();
-    ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, true);
-    ctx.fill();
+        canvas.width = 480;
+        canvas.here = 320;
+
+        ctx.fillStyle = 'black';
+        ctx.beginPath();
+        ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, true);
+        ctx.fill();
+    }
+
+    return {
+        start: start
+    }
 }) ();
+
+gameModule.start();
