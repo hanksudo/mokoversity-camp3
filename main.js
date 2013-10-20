27afbd4d37e12bbb21d51e102511a35aeca8a79a
Hank Wang <drapho@gmail.com>
2013-10-20 10:30:53 +0800

    handler input event. fixes #10

--- main.js
+++ main.js
@@ -2,13 +2,25 @@ var gameModule = (function() {
 
     var counter = 0;
 
+    function touchEvent(evt) {
+        var x = evt.clientX,
+            y = evt.clientY;
+
+        console.log('Clicked ' + x + ", " + y);
+    }
+
     function start() {
+        document.getElementById('main').addEventListener('click', touchEvent, false);
+        startGame();
+    }
+
+    function startGame() {
         var canvas = document.getElementById('game');
         var ctx = canvas.getContext('2d');
 
         var ballX = Math.floor(Math.random() * 500);
         var ballY = Math.floor(Math.random() * 300);
-        var ballR = Math.floor(Math.random() * 120);
+        var ballR = Math.floor(Math.random() * 150);
 
         canvas.width = 640;
         canvas.height = 360;
