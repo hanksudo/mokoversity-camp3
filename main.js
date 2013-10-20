f02db991e8f7e1878101eda043463b2b5608dd11
Hank Wang <drapho@gmail.com>
2013-10-20 10:02:52 +0800

    產生十個球. fixes #8

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
 var gameModule = (function() {
 
+    var counter = 0;
+
     function start() {
         var canvas = document.getElementById('game');
         var ctx = canvas.getContext('2d');
@@ -15,6 +17,18 @@ var gameModule = (function() {
         ctx.beginPath();
         ctx.arc(ballX, ballY, ballR, 0, Math.PI * 2, true);
         ctx.fill();
+
+        if (counter >= 9) {
+            gameOver();
+        } else {
+            counter++;
+            console.log('Counter: ' + counter);
+            setTimeout(start, 1000);
+        }
+    }
+
+    function gameOver () {
+        console.log('Game Over!');
     }
 
     return {
